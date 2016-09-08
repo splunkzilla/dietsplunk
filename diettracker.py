f642fb34f6fdc935e869b154dfce5f861932fb90
Justin Boucher <jboucher@splunk.com>
2016-09-08 07:45:41 -0700

    Initial commit

---
+++ diettracker.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+# Splunk Diet Tracker
+# Splunk Bioelectrical Tracking Experiment
+# This application was made for use with a Makey Makey kit http://makeymakey.com
+# https://github.com/splunkzilla/dietsplunk
+#
+# (c) 2016 Justin Boucher
+
+import sys
+import pygame
+from pygame.locals import *
+import pygame.mixer
+
+if __name__ == "__main__":
+	print('main function')
